4c092f2370455a6d590e809e87462a457debc6f2
Dirk Arnez <smalldirkalex@gmail.com>
2023-11-08 18:14:56 +0800

    - update make-portable-installation.cmd

--- make-portable-installation.cmd
+++ make-portable-installation.cmd
@@ -1,6 +1,7 @@
 set PATH=%PATH%;
 
 cmd /c ""install-tl-windows.bat" --help"
-cmd /c ""install-tl-windows.bat" --paper=a4 -no-gui -portable -scheme=minimal --no-interaction -texdir=%~dp0texlive/2023  -texmflocal=%~dp0texlive/texmf-local"
+@REM -scheme=minimal
+cmd /c ""install-tl-windows.bat" --paper=a4 -no-gui -portable --no-interaction -texdir=%~dp0texlive/2023  -texmflocal=%~dp0texlive/texmf-local"
 
 pause
